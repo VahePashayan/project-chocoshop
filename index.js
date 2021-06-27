d36a2fb70fa4e9ba2d86835bd5bfe6f9c8d3dd62
VahePashayan <vpashayan39gmail.com>
2021-06-27 15:17:31 +0400

    feat: added validation to contact us form

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-let chatbox = document.querySelector(".chatbox");
-chatbox.addEventListener("click", function() {
-    if(document.querySelector(".chatForm").style.visibility = "hidden") {
-        document.querySelector(".chatForm").style.visibility = "visible";
-    }else if(document.querySelector(".chatForm").style.visibility = "visible"){
-        document.querySelector(".chatForm").style.visibility = "hidden";
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let mess = document.querySelector("#mess");
+let btn = document.querySelector(".btn");
+
+let regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+name.addEventListener("input", () => {
+    if(name.value.length < 4 || !isNaN(name.value[0])) {
+        name.style.backgroundColor = "#ff9999";
+    }else {
+        name.style.backgroundColor = "white";
     }
 })
 
+email.addEventListener("input", () => {
+    if(!(regexEmail.test(email.value))) {
+        email.style.backgroundColor = "#ff9999";
+    }else {
+        email.style.backgroundColor = "white";
+    }
+})
